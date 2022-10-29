837fbc517a26eb123d4948358fd6ded81561075d
aliakseiv <1189185@gmail.com>
2022-10-29 13:08:33 +0200

    Final changes

--- src/Actions.java
+++ src/Actions.java
@@ -1,15 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Actions {
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
         String processor = "Intel";
         int ram = random.nextInt(32) + 1;
         int hdd = random.nextInt(5) + 1;
-        int resource = random.nextInt(50) + 1;
-        Computer myComputer = new Computer(processor,ram,hdd,resource);
+        int resource = random.nextInt(5);
+        Computer myComputer = new Computer(processor,ram,hdd,resource,number);
         myComputer.Info();
-
+        System.out.println("Введи число от 0 до 1 чтобы включить");
+        number = sc.nextInt();
+        myComputer.turnOn();
     }
 
 }
--- src/Computer.java
+++ src/Computer.java
@@ -5,6 +5,15 @@ public class Computer {
     int ram;
     int hdd;
     int resource;
+    int number;
+
+    public Computer(String processor, int ram, int hdd, int resource, int number) {
+        this.processor = processor;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.resource = resource;
+        this.number = number;
+    }
 
     public Computer(String processor, int ram, int hdd, int resource) {
         this.processor = processor;
@@ -18,5 +27,15 @@ public class Computer {
         System.out.println("RAM: " + ram + " Gb");
         System.out.println("HDD: " + hdd + " Tb");
         System.out.println("Resource: " + resource + " cycles");
+    }
+
+    public void turnOn() {
+        Random random = new Random();
+        int chance = random.nextInt(2);
+        if (number == chance && resource > 0) {
+            System.out.println("Компьютер включился");
+        } else {
+            System.out.println("Компьютер сгорел");
+        }
     }
 }
