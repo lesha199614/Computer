017ffd6cacbd3c7e19927403aa5216cb41d24777
aliakseiv <1189185@gmail.com>
2022-11-01 12:33:35 +0100

    Another solution

--- src/Actions.java
+++ src/Actions.java
@@ -5,16 +5,16 @@ public class Actions {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
-        int number = 0;
         String processor = "Intel";
         int ram = random.nextInt(32) + 1;
         int hdd = random.nextInt(5) + 1;
         int resource = random.nextInt(5);
-        Computer myComputer = new Computer(processor,ram,hdd,resource,number);
+        boolean isTurnedOn = false;
+        boolean isBurnedOut = false;
+        Computer myComputer = new Computer(processor,ram,hdd,resource,isTurnedOn,isBurnedOut);
         myComputer.Info();
-        System.out.println("Введи число от 0 до 1 чтобы включить");
-        number = sc.nextInt();
         myComputer.turnOn();
+        myComputer.turnOff();
     }
 
 }
